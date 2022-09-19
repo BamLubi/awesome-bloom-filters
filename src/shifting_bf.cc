6cc9f22433117bdf052c4c2dff0aafc88ade90ce
BamLubi-VM <1126458447@qq.com>
2022-09-19 23:18:49 +0800

    feat: support ShBF

--- src/shifting_bf.cc
+++ src/shifting_bf.cc
@@ -1,27 +1,54 @@
 #include "filter_policy.h"
+#include "hash.h"
 
 #include <iostream>
 #include <bitset>
 
+#define FILTER_SIZE_SHBF 1000000000 // 1e9的容量
+
 class ShiftingBloomFilterPolicy : public FilterPolicy
 {
 private:
+    std::bitset<FILTER_SIZE_SHBF + 25> bits; // m + c
+    size_t k_;                               // 使用的时候只有 k / 2
+    size_t w = 25;
+
 public:
+    ShiftingBloomFilterPolicy(size_t k) : k_(k)
+    {
+        bits.reset();
+    }
+
     const char *Name() const override { return "ShiftingBloomFilterPolicy"; }
 
     // override
-    void Add(const char* key) override
+    void Add(const char *key) override
     {
+        uint32_t offset = Hash(key, k_ / 2 + 1 - 1) % (w - 1) + 1;
+        for (int i = 0; i < k_ / 2; i++)
+        {
+            uint32_t h = Hash(key, i);
+            bits[h % FILTER_SIZE_SHBF] = 1;
+            bits[h % FILTER_SIZE_SHBF + offset] = 1;
+        }
     }
 
     // override
-    bool Contains(const char* key) const override
+    bool Contains(const char *key) const override
     {
+        uint32_t offset = Hash(key, k_ / 2 + 1 - 1) % (w - 1) + 1;
+        for (int i = 0; i < k_ / 2; i++)
+        {
+            uint32_t h = Hash(key, i);
+            if (!bits.test(h % FILTER_SIZE_SHBF) ||
+                !bits.test(h % FILTER_SIZE_SHBF + offset))
+                return false;
+        }
         return true;
     }
 };
 
-FilterPolicy *NewShiftingBloomFilterPolicy()
+FilterPolicy *NewShiftingBloomFilterPolicy(size_t k = 6)
 {
-    return new ShiftingBloomFilterPolicy();
+    return new ShiftingBloomFilterPolicy(k);
 }
