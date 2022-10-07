1b7f91da5e2980395086e82536691f474dd9b1cb
BamLubi-VM <1126458447@qq.com>
2022-10-07 23:58:50 +0800

    fix: update

--- src/hash.cc
+++ src/hash.cc
@@ -4,12 +4,14 @@
 #include <stdint.h>
 #include <stddef.h>
 
-const uint32_t seeds[30] = {12323,12401,12487,12553,12641,12739,12829,12923,13007,13109,
-    13187,13309,13411,13499,15149,15259,15319,15401,15497,15607,
-    15679,15773,15881,15971,16069,16183,16267,16381,16481,17891};
+const uint32_t seeds[30] = {12323, 12401, 12487, 12553, 12641, 12739, 12829, 12923, 13007, 13109,
+                            13187, 13309, 13411, 13499, 15149, 15259, 15319, 15401, 15497, 15607,
+                            15679, 15773, 15881, 15971, 16069, 16183, 16267, 16381, 16481, 17891};
 
-uint32_t Hash(const char *data, int seed_index) {
-    seed_index = seed_index < 0 ? 0 : seed_index > 29 ? 29 : seed_index;
+uint32_t Hash(const char *data, int seed_index)
+{
+    seed_index = seed_index < 0 ? 0 : seed_index > 29 ? 29
+                                                      : seed_index;
     uint32_t _hash = 1;
     uint32_t p = 1e9 + 7;
     uint32_t mod = 1610612741;
